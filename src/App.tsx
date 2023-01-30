b02c4afc2226a6ef56e1e119b8981c18d8c3d7b6
tayyabaaAwan <tayyaba.awan@hyfatech.com>
2023-01-30 15:05:27 +0500

    Monday Task

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,9 @@ import ICOJson from "./ico.json";
 function App() {
   const web3 = new Web3(window.ethereum);
   
-  const address="0x20d0C061153A60BC166b32a33d7828261AE9FA97";
-  const [checkAmount, setCheckAmount] = useState('Enter the amount here');
-  const [accounts, setAccount] = useState<any>([]);
+  const address="0xFB189474191B70584958f4AB104b9d1147ae5140";
+  const [checkAmount, setCheckAmount] = useState(0);
+  const [accountCheck, setAccountCheck] = useState<any>([]);
 
   async function connectWallet() {
     try {
@@ -19,51 +19,46 @@ function App() {
         else
           await window.ethereum.enable();
           let account = await web3.eth.getAccounts();
-          setAccount(account);
+          setAccountCheck(account);
         // chainId = await web3.eth.getChainId()
         console.log("address", account);
-        return accounts;
+        return account;
     } catch (error) {
         console.log(error);
     }
   }
-  // const Account=()=> {
-  //   setAccount(accounts[0]);
-  //   console.log(accounts[0])
-  //   return(accounts[0])
-  // }
-   
+  
 const Amount=()=> {
   setCheckAmount(checkAmount);
   console.log(checkAmount)
   return(checkAmount)
 }
-  // async function Funds(amount : any) {
-  //   await invest(amount);
-  // };
 
- async function invest() {
+ async function invest(amount:any) {
   try {
+    connectWallet()
+    console.log(amount,"checkamount1")
     const contract = new web3.eth.Contract(ICOJson.abi as AbiItem[], address);
-    const funds = await contract.methods.invest().send({from :accounts[0]}); 
+    console.log(amount,"2")
+    const funds = await contract.methods.invest().send({from :accountCheck[0], value:amount}); 
+    console.log(amount,"3")
     console.log("Funds recieved", funds)
  } catch (error) {
     console.log(error)
  }
  }
-
+ async function userCollect(){
+  await invest(checkAmount)
+ }
 async function collectProfit() {
   try {
     const contract = new web3.eth.Contract(ICOJson.abi as AbiItem[], address);
-    const profit = await contract.methods.collectProfit().call(accounts[0]); 
+    const profit = await contract.methods.collectProfit().send({from:accountCheck[0]}); 
     console.log("Profit Collected: ", profit)
  } catch (error) {
     console.log(error)
  }
  }
- async function profitCollected(amount: any) {
-  await collectProfit();
-};
 return (
     <>
     <div className="App">
@@ -72,11 +67,11 @@ return (
       <form>
         <div className="form-group">
           <label htmlFor="exampleInputAmount">Invest in ICO</label><br />
-          <input className="form-control" placeholder="Add amount to invest in ICO" onChange={event => setCheckAmount(event.target.value)}></input>
+          <input className="form-control" placeholder="Add amount to invest in ICO" onChange={(event:any) => setCheckAmount(event.target.value)}></input>
         </div>
       </form>
-      <button type="submit" className="btn btn-primary" onClick={() => invest() }>Invest</button>
-      <button type="submit" className="btn btn-primary" onClick={() => profitCollected(checkAmount) }>Collect Profit</button>
+      <button type="submit" className="btn btn-primary" onClick={() => invest(checkAmount) }>Invest</button>
+      <button type="submit" className="btn btn-primary" onClick={() => collectProfit() }>Collect Profit</button> 
       </header>
     </div>  
     </>  
@@ -84,5 +79,5 @@ return (
  }
 
 export default App;
-// /<button onClick={getTokens}>Invest in ICO</button>
+
 
